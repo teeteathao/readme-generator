35b0ec2e6eee3ba5a6394586351f64fc52ff9376
teeteathao <87237312+teeteathao@users.noreply.github.com>
2021-11-12 19:25:36 -0800

    added package lines to js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      message: "What is title of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What are the sections?",
+      name: "section",
+    },
+    {
+        type: "input",
+        message: "Table of Content?",
+        name: "lang",
+    },
+    {
+        type: "input",
+        message: "Installation?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Usage?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "License?",
+        name: "license",
+    },
+    {
+        type: "input",
+        message: "Contributing?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "Tests?",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "Questions?",
+        name: "questions",
+      },
+  ]
+
+// title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
